c1f43903a4dec885ff3b0dd2a3bae7f06d919bb1
Wen-Chun Lin <lovecankill@gmail.com>
2017-04-14 16:41:36 +0800

    Use pipe

--- src/app/mask.pipe.ts
+++ src/app/mask.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MaskPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string, maxlength: number = 4): any {
+    if (value.length > maxlength) {
+      return value.substr(0, maxlength) + "***";
+    } else {
+      return value;
+    }
   }
 
 }
