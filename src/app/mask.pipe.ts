cb8a5a7b98d8d47f1d892e404562bef3cbf193ba
Wen-Chun Lin <lovecankill@gmail.com>
2017-04-14 16:55:49 +0800

    Generate star by max length

--- src/app/mask.pipe.ts
+++ src/app/mask.pipe.ts
@@ -6,8 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MaskPipe implements PipeTransform {
 
   transform(value: string, maxlength: number = 4): any {
+    let mask_star = '';
+    for (let i = 0; i < maxlength; i++) {
+      mask_star += "*";
+    }
     if (value.length > maxlength) {
-      return value.substr(0, maxlength) + "***";
+      return value.substr(0, maxlength) + mask_star;
     } else {
       return value;
     }
